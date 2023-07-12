0d6c074ee5c967e65154338ff81a6f6ee3b92739
Dat Tan <dattan2911@gmail.com>
2023-07-12 16:50:29 +0700

    fix error

--- server.js
+++ server.js
@@ -23,13 +23,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
